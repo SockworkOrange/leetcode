462a8608c15eb649d7bdc8d23ab8e5d799d8e2df
Ilan Uzan <ilan9uzan@gmail.com>
2025-01-22 22:59:00 +0100

    more fixes

--- generate_parenthesis/solution.py
+++ generate_parenthesis/solution.py
@@ -10,8 +10,8 @@ class Solution:
 
     def expand_permutations(self, strs: dict[str, int], depth: int, n: int) -> dict[str, int]:
         should_expand = False
-
-        strs_k = list(strs)
+        strs_k = [s for s, i in strs.items() if n - i > 0]
+        strs_k.sort(key=lambda s: len(s))
         strs_to_expand = [s for s, i in strs.items() if i == depth]
         for s1 in strs_to_expand:
             i = strs[s1]
